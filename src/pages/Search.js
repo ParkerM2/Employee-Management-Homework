061c816edc8fc574d39a0466e425238077d2aed0
Parker Manning <parkerlmanning@hotmail.com>
2020-12-26 13:25:04 -0600

    api call is console logging, switching to functional components

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import API from "./../utils/API";
 import SearchResults from "../components/SearchResults";
 import Container from "../components/Container";
@@ -9,58 +9,58 @@ import SearchForm from "../components/SearchForm";
 import Alert from "../components/Alert"
 // import UserInfo from "../components/UserInfo";
 
-class Search extends Component {
-  state = {
-    search: "",
-    breeds: [],
-    results: [],
-    error: ""
-  };
+const Search = props => {
 
+  const [user, setUser] = useState({
+    user: [],
+  })
+  const [userImg, setUserImg] = useState({
+    userImg: [],
+  })
+  const [userGender, setUserGender] = useState({
+    userGender: [],
+  })
   // When the component mounts, get a list of all available base breeds and update this.state.breeds
-  componentDidMount() {
-    API.getRandomEmployee()
-      .then(res => console.log(`This is api repsonse ${res}`))
-      // .then(res => this.setState({ breeds: res.data.message }))
+  useEffect(() => {
+    console.log("useEffect runs")
+    API.getUserList()
+      .then(res => console.log(`This is api repsonse`, res.data))
+      .then(res => setUser({user: res.data}))
       .catch(err => console.log(err));
-  }
+  },[])
 
-  handleInputChange = event => {
-    this.setState({ search: event.target.value });
-  };
+  // const handleInputChange = event => {
+  //   this.setState({ search: event.target.value });
+  // };
 
-  handleFormSubmit = event => {
-    event.preventDefault();
-    API.getRandomEmployee(this.state.search)
-      .then(res => {
-        if (res.data.status === "error") {
-          throw new Error(res.data.message);
-        }
-        this.setState({ results: res.data.message, error: "" });
-      })
-      .catch(err => this.setState({ error: err.message }));
-  };
-  render() {
+  // handleFormSubmit = event => {
+  //   event.preventDefault();
+  //   API.getRandomEmployee(this.state.search)
+  //     .then(res => {
+  //       if (res.data.status === "error") {
+  //         throw new Error(res.data.message);
+  //       }
+  //       this.setState({ results: res.data.message, error: "" });
+  //     })
+  //     .catch(err => this.setState({ error: err.message }));
+  // };
+ 
     return (
       <div>
         <Container style={{ minHeight: "80%" }}>
           <h1 className="text-center">Search By Breed!</h1>
-          <Alert
-            type="danger"
-            style={{ opacity: this.state.error ? 1 : 0, marginBottom: 10 }}
-          >
-            {this.state.error}
-          </Alert>
-          <SearchForm
+          <h1>{user.user}</h1>
+        
+          {/* <SearchForm
             handleFormSubmit={this.handleFormSubmit}
             handleInputChange={this.handleInputChange}
             breeds={this.state.breeds}
-          />
-          <SearchResults results={this.state.results} />
+          /> */}
+          {/* <SearchResults user={user} userImg={userImg} userGender={userGender} /> */}
         </Container>
       </div>
     );
   }
-}
+
 
 export default Search;
